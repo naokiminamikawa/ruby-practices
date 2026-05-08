9384928ddbc86e7e1d90667635f240bd41122ea0
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-05-08 14:13:52 +0000

    fizzbuzzのプログラム作成

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,12 @@
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+list.each do |number|
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
+    puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
+  else
+    puts number
+  end
+end
