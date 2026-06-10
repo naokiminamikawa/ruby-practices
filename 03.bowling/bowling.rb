ab82deb8e81eaaa12ff8083a831e9844fdc642d4
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-06-10 01:40:13 +0000

    ボーリングプログラム修正2回目

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+
 input_score = ARGV[0]
 score_tokens = input_score.split(',')
 
@@ -12,22 +13,22 @@ roll_index = 0
 total_score = 10.times.sum do
   if rolls[roll_index] == 10
     # ストライク
-    score = 10 + rolls[roll_index + 1, 2].sum
+    score = rolls[roll_index] + rolls[roll_index + 1, 2].sum
     roll_index += 1
-    score
   else
     frame_total = rolls[roll_index, 2].sum
 
     score = if frame_total == 10
-                     # スペア
-                     10 + rolls[roll_index + 2]
-                   else
-                     frame_total
-                   end
+              # スペア
+              10 + rolls[roll_index + 2]
+            else
+              frame_total
+            end
 
     roll_index += 2
-    score
   end
+
+  score
 end
 
 puts total_score
