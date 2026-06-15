024e5692a392794561e45db9cb499ee2c942c8b0
NaokiMinamikawa <naoki.minamikawa@omron.com>
2026-06-15 22:43:22 +0000

    スペア・ストライクの計算式修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# frozen_string_literal: true
 
 input_score = ARGV[0]
 score_tokens = input_score.split(',')
@@ -13,14 +14,14 @@ roll_index = 0
 total_score = 10.times.sum do
   if rolls[roll_index] == 10
     # ストライク
-    score = rolls[roll_index] + rolls[roll_index + 1, 2].sum
+    score = rolls[roll_index, 3].sum
     roll_index += 1
   else
     frame_total = rolls[roll_index, 2].sum
 
     score = if frame_total == 10
               # スペア
-              10 + rolls[roll_index + 2]
+              rolls[roll_index, 3].sum
             else
               frame_total
             end
